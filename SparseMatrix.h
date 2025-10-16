64a86467b826c88d85a3ec63e99b7f5af60d3c1d
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-10-16 12:24:34 -0300

    Update SparseMatrix.h

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -5,9 +5,9 @@ class SparseMatrix {
   Node* start;
   public:
     SparseMatrix();
-    void add(int value, int xPos, int yPos);
-    int get(int xPos, int yPos);
-    void remove(int xPos, int yPos);
+    void add(int valor, int posX, int posY);
+    int get(int posX, int posY);
+    void remove(int posX, int posY);
     void printStoredValues();
     int density();  
     SparseMatrix* multiply(SparseMatrix* second);
