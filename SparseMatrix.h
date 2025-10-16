6138e0a72da46d55f58e4ef3259572cf344d287f
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-10-16 11:38:14 -0300

    SparseMatrix.h

---
+++ SparseMatrix.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Node.h"
+class SparseMatrix {
+  private:
+  Node* start;
+  public:
+    SparseMatrix();
+    void add(int value, int xPos, int yPos);
+    int get(int xPos, int yPos);
+    void remove(int xPos, int yPos);
+    void printStoredValues();
+    int density();  
+    SparseMatrix* multiply(SparseMatrix* second);
+
+    ~SparseMatrix();
+};
