1fe3e63fb24d1da5f0645dfabb9d41caccd95d4f
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-10-22 09:22:24 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,12 @@ int main() {
   while (menu != 9)
     {
         cout << "========================\n";
-        cout << "1.- Gestión de alumnos\n";
-        cout << "2.- Gestión de cursos\n";
-        cout << "3.- Inscripción de cursos\n";
-        cout << "4.- Gestión de notas\n";
-        cout << "5.- Reportes\n";
+        cout << "1.- Añadir nuevo nodo\n";
+        cout << "2.- Obtener un valor\n";
+        cout << "3.- Eliminar un nodo\n";
+        cout << "4.- Imprimir valores\n";
+        cout << "5.- Calcular la densidad\n";
+        cout << "6.- Multiplicar la matriz\n";
         cout << "9.- Salir\n";
         cout << "========================\n";
         cout << "Ingrese una opción: ";
@@ -21,22 +22,89 @@ int main() {
 
         switch (menu)
         {
-        case 1:
-            Caso1();
+        case 1: //agregar
+            int valor = 0;
+            int posx = 0;
+            int posy = 0;
+            cout<< "\nIngrese un valor: ";
+            cin >> valor;
+            cout<< "\nIngrese la posición x: ";
+            cin >> posx;
+            cout<< "\nIngrese la posición y: ";
+            cin >> posy;
+          
+            std::clock_t inicio = std::clock();
+          
+            funciones.add(valor, posx, posy);
+          
+            std::clock_t fin = std::clock();
+            double tiempoTranscurrido = (double)(fin - inicio) / CLOCKS_PER_SEC;
+            std::cout << "Tiempo de CPU consumido: " << tiempoTranscurrido << " segundos" << std::endl;
             break;
-        case 2:
-            Caso2();
+          
+        case 2: //obtener
+            int posx = 0;
+            int posy = 0;
+            cout<< "\nIngrese la posición x: ";
+            cin >> posx;
+            cout<< "\nIngrese la posición y:  ";
+            cin >> posy;
+          
+            std::clock_t inicio = std::clock();
+          
+            funciones.get(posx, posy);
+          
+            std::clock_t fin = std::clock();
+            double tiempoTranscurrido = (double)(fin - inicio) / CLOCKS_PER_SEC;
+            std::cout << "Tiempo de CPU consumido: " << tiempoTranscurrido << " segundos" << std::endl;
             break;
-        case 3:
-  
-  /*std::clock_t inicio = std::clock();
-  //llamar las disntintas funciones e ir agregando los datos
-  funciones.add(50, 0, 0);
-  
-  std::clock_t fin = std::clock();
-  double tiempoTranscurrido = (double)(fin - inicio) / CLOCKS_PER_SEC;
-  std::cout << "Tiempo de CPU consumido: " << tiempoTranscurrido << " segundos" << std::endl;
-    */
+        case 3: //Eliminar
+            int posx = 0;
+            int posy = 0;
+            cout<< "\nIngrese la posición x: ";
+            cin >> posx;
+            cout<< "\nIngrese la posición y:  ";
+            cin >> posy;
+          
+            std::clock_t inicio = std::clock();
+          
+            funciones.remove(posx, posy);
+          
+            std::clock_t fin = std::clock();
+            double tiempoTranscurrido = (double)(fin - inicio) / CLOCKS_PER_SEC;
+            std::cout << "Tiempo de CPU consumido: " << tiempoTranscurrido << " segundos" << std::endl;
+            break;
+          
+        case 4: //Imprimir
+            std::clock_t inicio = std::clock();
+          
+            funciones.printStoredValues();
+          
+            std::clock_t fin = std::clock();
+            double tiempoTranscurrido = (double)(fin - inicio) / CLOCKS_PER_SEC;
+            std::cout << "Tiempo de CPU consumido: " << tiempoTranscurrido << " segundos" << std::endl;
+          
+            break;
+        case 5: //densidad
+            std::clock_t inicio = std::clock();
+          
+            funciones.density();
+          
+            std::clock_t fin = std::clock();
+            double tiempoTranscurrido = (double)(fin - inicio) / CLOCKS_PER_SEC;
+            std::cout << "Tiempo de CPU consumido: " << tiempoTranscurrido << " segundos" << std::endl;
+          
+            break;
+        case 6: //multiplicación
+            break;
+        case 9:
+            cout << "Saliendo...\n";
+            break;
+        default:
+            cout << "Ingrese una opcion valida\n";
+            break;
+        }
+    }
   
   return 0;
 }
