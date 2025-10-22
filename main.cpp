eb12d222343ed346804f840c9b387425cba4a03f
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-10-22 08:14:42 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 #include <ctime>
+using namespace std;
+SparseMatrix funciones;
+
+int main() {
+  std::clock_t inicio = std::clock();
+  //llamar las disntintas funciones e ir agregando los datos
+  std::clock_t fin = std::clock();
+  double tiempoTranscurrido = (double)(fin - inicio) / CLOCKS_PER_SEC;
+  std::cout << "Tiempo de CPU consumido: " << tiempoTranscurrido << " segundos" << std::endl;
+  
+  return 0;
+}
