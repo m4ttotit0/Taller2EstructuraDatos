c411b097a3e3e4a12b3f91eb2275c5dd210d26e2
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-10-22 08:58:53 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-SparseMatrix funciones;
 
 int main() {
-  std::clock_t inicio = std::clock();
+  SparseMatrix funciones; //Creo la variable referente a la clase SparseMatrix.cpp (I hope so)
+  
+  int menu;
+  
+  
+  /*std::clock_t inicio = std::clock();
   //llamar las disntintas funciones e ir agregando los datos
+  funciones.add(50, 0, 0);
+  
   std::clock_t fin = std::clock();
   double tiempoTranscurrido = (double)(fin - inicio) / CLOCKS_PER_SEC;
   std::cout << "Tiempo de CPU consumido: " << tiempoTranscurrido << " segundos" << std::endl;
+    */
   
   return 0;
 }
