5694cf5df41c950d58947946e51805ee0a266099
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-10-23 09:31:49 -0300

    Update SparseMatrix.h

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -1,12 +1,25 @@
 #pragma once
 #include "Node.h"
+
+struct NodeHead {
+    int index;
+    Node* first;
+    NodeHead* next;
+    
+    NodeHead(int idx) : index(idx), first(nullptr), next(nullptr) {}
+};
+
 class SparseMatrix {
-  private:
-  Node* start;
-  int maxRow;
-  int maxCol;
-  int count;
-  public:
+private:
+    NodeHead* rowHeads;
+    NodeHead* colHeads;
+    int maxRow;
+    int maxCol;
+    int count;
+    NodeHead* findOrCreateHead(NodeHead*& headList, int index, int& maxDim);
+    NodeHead* findHead(NodeHead* headList, int index);
+    
+public:
     SparseMatrix();
     void add(int value, int xPos, int yPos);
     int get(int xPos, int yPos);
@@ -14,6 +27,10 @@ class SparseMatrix {
     void printStoredValues();
     int density();  
     SparseMatrix* multiply(SparseMatrix* second);
+
+    int getElement(int xPos, int yPos);
+    int getRowCount() const { return maxRow; }
+    int getColCount() const { return maxCol; }
 
     ~SparseMatrix();
 };
