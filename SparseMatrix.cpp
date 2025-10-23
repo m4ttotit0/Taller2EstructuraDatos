b9dcd2a41e51fb9c246ec221b95830d4b22b7843
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2025-10-23 09:40:57 -0300

    Update SparseMatrix.cpp

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -1,128 +1,264 @@
 #include "SparseMatrix.h"
 #include <iostream>
+
 using namespace std;
 
-SparseMatrix::SparseMatrix() : start(nullptr), maxRow(0), maxCol(0), count(0) {}
+SparseMatrix::SparseMatrix() : rowHeads(nullptr), colHeads(nullptr), maxRow(0), maxCol(0), count(0) {}
 
-SparseMatrix::~SparseMatrix() {}
+SparseMatrix::~SparseMatrix() {
+    NodeHead* currentRowHead = rowHeads;
+    while (currentRowHead != nullptr) {
+        Node* current = currentRowHead->first;
+        while (current != nullptr) {
+            Node* next = current->nextRow;
+            delete current;
+            count--;
+            current = next;
+        }
+        currentRowHead = currentRowHead->next;
+    }
+
+    NodeHead* currentHead = rowHeads;
+    while (currentHead != nullptr) {
+        NodeHead* nextHead = currentHead->next;
+        delete currentHead;
+        currentHead = nextHead;
+    }
+
+    currentHead = colHeads;
+    while (currentHead != nullptr) {
+        NodeHead* nextHead = currentHead->next;
+        delete currentHead;
+        currentHead = nextHead;
+    }
+}
+
+NodeHead* SparseMatrix::findOrCreateHead(NodeHead*& headList, int index, int& maxDim) {
+    NodeHead* current = headList;
+    NodeHead* prev = nullptr;
+
+    while (current != nullptr && current->index < index) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current != nullptr && current->index == index) {
+        return current;
+    }
+
+    NodeHead* newHead = new NodeHead(index);
+    if (prev == nullptr) {
+        newHead->next = headList;
+        headList = newHead;
+    } else {
+        newHead->next = current;
+        prev->next = newHead;
+    }
+
+    if (index > maxDim) {
+        maxDim = index;
+    }
+
+    return newHead;
+}
+
+NodeHead* SparseMatrix::findHead(NodeHead* headList, int index) {
+    NodeHead* current = headList;
+    while (current != nullptr && current->index < index) {
+        current = current->next;
+    }
+    if (current != nullptr && current->index == index) {
+        return current;
+    }
+    return nullptr;
+}
 
 void SparseMatrix::add(int value, int xPos, int yPos)
 {
-    if (value == 0)
+    if (value == 0) {
+        remove(xPos, yPos);
         return;
+    }
+
+    NodeHead* rowH = findOrCreateHead(rowHeads, xPos, maxRow);
+    NodeHead* colH = findOrCreateHead(colHeads, yPos, maxCol);
+    
+    Node* currentR = rowH->first;
+    Node* prevR = nullptr;
+
+    Node* currentC = colH->first;
+    Node* prevC = nullptr;
+
+    while (currentR != nullptr && currentR->yPos < yPos) {
+        prevR = currentR;
+        currentR = currentR->nextRow;
+    }
+
+    if (currentR != nullptr && currentR->yPos == yPos) {
+        currentR->value = value;
+        return;
+    }
 
     Node *newNode = new Node(value, xPos, yPos);
-
-    // Si es que no hay nodo, sera el inicial
-    if (start == nullptr)
-    {
-        start = newNode;
-        count++;
-        if (xPos > maxRow)
-            maxRow = xPos;
-        if (yPos > maxCol)
-            maxCol = yPos;
-        return;
-    }
-
-    // Se busca la posicion (x,y)
-    Node *current = start;
-    Node *prev = nullptr;
-
-    while (current != nullptr &&
-           (current->xPos < xPos ||
-            (current->xPos == xPos && current->yPos < yPos)))
-    {
-        prev = current;
-        current = current->nextCol; // recorremos en x
-    }
-
-    // Actualizamos el nodo si es que existe
-    if (current != nullptr && current->xPos == xPos && current->yPos == yPos)
-    {
-        current->value = value;
-        return;
-    }
-
-    // Insertamos el nuevo nodo
-    if (prev == nullptr)
-    {
-        newNode->nextCol = start;
-        start = newNode;
-    }
-    else
-    {
-        newNode->nextCol = prev->nextCol;
-        prev->nextCol = newNode;
-    }
-
     count++;
-    if (xPos > maxRow)
-        maxRow = xPos;
-    if (yPos > maxCol)
-        maxCol = yPos;
-}
 
-void SparseMatrix::remove(int xPos, int yPos)
-{
-    if (start == nullptr)
-        return;
-
-    Node *current = start;
-    Node *prev = nullptr;
-
-    while (current != nullptr &&
-           (current->xPos != xPos || current->yPos != yPos))
-    {
-        prev = current;
-        current = current->nextCol;
+    if (prevR == nullptr) {
+        newNode->nextRow = rowH->first;
+        rowH->first = newNode;
+    } else {
+        newNode->nextRow = prevR->nextRow;
+        prevR->nextRow = newNode;
     }
-
-    // Retorna si no encuentra resultado
-    if (current == nullptr)
-        return;
-
-    // Si es el primero
-    if (prev == nullptr)
-    {
-        start = current->nextCol;
+    
+    while (currentC != nullptr && currentC->xPos < xPos) {
+        prevC = currentC;
+        currentC = currentC->nextCol;
     }
-    else
-    {
-        prev->nextCol = current->nextCol;
+    
+    if (prevC == nullptr) {
+        newNode->nextCol = colH->first;
+        colH->first = newNode;
+    } else {
+        newNode->nextCol = prevC->nextCol;
+        prevC->nextCol = newNode;
     }
-
-    delete current;
-    count--;
 }
 
 int SparseMatrix::get(int xPos, int yPos)
 {
-    Node *current = start;
+    NodeHead* rowH = findHead(rowHeads, xPos);
+    if (rowH == nullptr) return 0; 
+
+
+    Node *current = rowH->first;
     while (current != nullptr)
     {
-        if (current->xPos == xPos && current->yPos == yPos)
+        if (current->yPos == yPos)
             return current->value;
-        current = current->nextCol;
+
+        if (current->yPos > yPos) 
+            break; 
+        current = current->nextRow;
     }
-    return 0; // Si no existe retornamos 0
+    return 0;
 }
 
-void SparseMatrix::printStoredValues()
+
+void SparseMatrix::remove(int xPos, int yPos)
 {
-    Node *current = start;
-    while (current != nullptr)
+
+    NodeHead* rowH = findHead(rowHeads, xPos);
+    NodeHead* colH = findHead(colHeads, yPos);
+
+    if (rowH == nullptr || colH == nullptr) return; 
+    
+    Node *currentR = rowH->first;
+    Node *prevR = nullptr;
+    while (currentR != nullptr && currentR->yPos < yPos) {
+        prevR = currentR;
+        currentR = currentR->nextRow;
+    }
+    
+    if (currentR == nullptr || currentR->yPos != yPos) 
+        return; 
+
+    Node *currentC = colH->first;
+    Node *prevC = nullptr;
+    while (currentC != nullptr && currentC->xPos < xPos) {
+        prevC = currentC;
+        currentC = currentC->nextCol;
+    }
+    
+    if (currentC == nullptr || currentC->xPos != xPos)
+        return;
+
+    if (prevR == nullptr) {
+        rowH->first = currentR->nextRow;
+    } else {
+        prevR->nextRow = currentR->nextRow;
+    }
+
+    if (prevC == nullptr) {
+        colH->first = currentC->nextCol;
+    } else {
+        prevC->nextCol = currentC->nextCol;
+    }
+
+    delete currentR;
+    count--;
+
+}
+
+void SparseMatrix::printStoredValues() {
+    NodeHead *currentRowHead = rowHeads;
+    
+    while (currentRowHead != nullptr)
     {
-        cout << "(" << current->xPos << ", " << current->yPos << ") --> "
-             << current->value << endl;
-        current = current->nextCol;
+        Node *current = currentRowHead->first;
+        while (current != nullptr)
+        {
+            cout << "(" << current->xPos << ", " << current->yPos << ") --> "
+                 << current->value << endl;
+            current = current->nextRow;
+        }
+        currentRowHead = currentRowHead->next;
     }
 }
-int SparseMatrix::density()
-{
+
+
+int SparseMatrix::density() {
     int totalCells = (maxRow + 1) * (maxCol + 1);
     if (totalCells == 0)
         return 0;
+    
     return (int)(((double)count * 100.0) / totalCells);
-    // En pocas palabras sacamos el porcentaje de numeros existentes en el total
+}
+
+
+SparseMatrix* SparseMatrix::multiply(SparseMatrix* second) {
+    if (maxCol != second->getRowCount()) {
+        return nullptr;
+    }
+
+    SparseMatrix* result = new SparseMatrix();
+
+    NodeHead* rowA = rowHeads;
+    while (rowA != nullptr) {
+        int i = rowA->index;
+
+        NodeHead* colB = second->colHeads;
+        while (colB != nullptr) {
+            int j = colB->index;
+            
+            int sum = 0;
+            
+            Node* currentA = rowA->first;
+            Node* currentB = colB->first;
+             
+            while (currentA != nullptr && currentB != nullptr) {
+                
+                int kA = currentA->yPos;
+                int kB = currentB->xPos;
+
+                if (kA == kB) {
+                    sum += currentA->value * currentB->value;
+                    currentA = currentA->nextRow;
+                    currentB = currentB->nextCol;
+                } else if (kA < kB) {
+                    currentA = currentA->nextRow;
+                } else { 
+                    currentB = currentB->nextCol;
+                }
+            }
+            
+            if (sum != 0) {
+                result->add(sum, i, j);
+            }
+            
+            colB = colB->next;
+        }
+        rowA = rowA->next;
+    }
+
+    return result;
 }
